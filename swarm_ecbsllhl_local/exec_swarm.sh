0d8bdc2a83d7680b3d5ac2d48947e76e4ed1b2cf
Takumi Yajima <Takumi.Yajima@sony.com>
2021-05-28 08:19:15 +0000

    add local

--- swarm_ecbsllhl_local/exec_swarm.sh
+++ swarm_ecbsllhl_local/exec_swarm.sh
@@ -18,7 +18,8 @@ SIM=/home/ubuntu/github/swarm_simvp/build/opt/trace_sim/sim
 #${SIM} -config ${CONFIG_DIR}/8c.cfg -- ./ecbs_simple_swarm -i ${EXAMPLES}/map_32by32_obst204_agents20_ex16.yaml -o output.yaml -w 1.1
 
 #${SIM} -config ${CONFIG_DIR}/2c.cfg -- /home/ubuntu/github/swarm_ecbsllhl/source_codes/ecbs_simple_swarm -i ${EXAMPLES}/map_8by8_obst12_agents10_ex13.yaml -o output.yaml -w 1.1
-${SIM} -config ${CONFIG_DIR}/64c.cfg -- /home/ubuntu/github/swarm_ecbsllhl_local/source_codes/ecbs_simple_swarm -i ${EXAMPLES}/map_32by32_obst204_agents20_ex11.yaml -o output.yaml -w 1
+${SIM} -config ${CONFIG_DIR}/4c.cfg -- /home/ubuntu/github/swarm_ecbsllhl_local/source_codes/ecbs_simple_swarm -i ${EXAMPLES}/map_32by32_obst204_agents20_ex1.yaml -o output.yaml -w 1.2
+
 #${SIM} -config ${CONFIG_DIR}/4c.cfg -- ./ecbs_simple_swarm -i ${EXAMPLES}/map_8by8_obst12_agents10_ex13.yaml -o output.yaml -w 1.1
 #${SIM} -config ${CONFIG_DIR}/64c.cfg -- ./ecbs_simple_swarm -i ${EXAMPLES}/map_8by8_obst12_agents10_ex2.yaml -o output.yaml -w 1.1
 
